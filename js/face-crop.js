f3294c3ef21c991ba2234dde5236da89846fd683
Hamish <leithhamish@gmail.com>
2021-03-28 11:38:10 +0100

    a
    a

--- js/face-crop.js
+++ js/face-crop.js
@@ -106,3 +106,16 @@ function exportAll() {
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+// JSZip
+
+var zip = new JSZip();
+zip.file("Hello.txt", "Hello World\n");
+
+var img = zip.folder("images");
+img.file("smile.gif", imgData, {base64: true});
+zip.generateAsync({type:"blob"})
+.then(function(content) {
+    // see FileSaver.js
+    saveAs(content, "example.zip");
+});
